8000f89266e86012cb7e032188151cc97d4c2530
Leonardo Ramos <Leo@Leonardos-MBP.iptv.microsoft.com>
2017-10-09 23:36:51 -0300

    assigment 2

---
+++ assignment2/scripts/app.js
@@ -0,0 +1,54 @@
+(function (){
+
+'use strict';
+
+angular.module('ShoppingCart', [])
+
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+	var toBuy = this;
+	toBuy.toBuyList = ShoppingListCheckOffService.getToBuyList();	
+	toBuy.bought = function (index, name, quantity) {
+		ShoppingListCheckOffService.manageLists(index, name, quantity);
+	}
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+	var alreadyBought = this;
+	alreadyBought.boughtList = ShoppingListCheckOffService.getToBoughtList();
+}
+
+function ShoppingListCheckOffService() {
+	var service = this;
+
+	var toBuyList = [
+		{ name: "cookies", quantity: 10 },
+		{ name: "cokes", quantity: 5 },
+		{ name: "kimchi", quantity: 20 },
+		{ name: "milk", quantity: 3 },
+		{ name: "cheese", quantity: 30 }
+	];
+
+	var boughtList = [];
+
+	service.getToBuyList = function () {
+    	return toBuyList;
+  	};
+
+  	service.getToBoughtList = function () {
+  		return boughtList;
+  	};
+
+	service.manageLists = function (itemIdex, name, quantity) {
+		toBuyList.splice(itemIdex, 1);
+		boughtList.push({name: name, quantity: quantity});
+	};
+	
+}
+
+})();
